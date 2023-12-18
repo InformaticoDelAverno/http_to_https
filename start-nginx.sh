76b50f4469a27c8d65c782f445adf4512029d3e6
Informatico Del Averno <informaticodelaverno@gmail.com>
2023-12-18 11:56:05 +0100

    first upload

--- start-nginx.sh
+++ start-nginx.sh
@@ -2,6 +2,6 @@
 
 # Reemplazar las variables de entorno en el template y crear nginx.conf.template
 envsubst '$PROXY_PASS_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
+cat /etc/nginx/nginx.conf
 # Iniciar Nginx
 nginx -g 'daemon off;'
